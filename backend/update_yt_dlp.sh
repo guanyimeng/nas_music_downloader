1f795c0dc696d6251766cf8a7e4e747e31c995d1
Nick Meng <mgy0822@gmail.com>
2026-02-03 01:53:46 -0500

    fix: correct file reference in update script for yt-dlp

--- backend/update_yt_dlp.sh
+++ backend/update_yt_dlp.sh
@@ -6,10 +6,10 @@ cd /home/dolphins22/Documents/Dev/nas_music_downloader/backend/
 poetry update yt-dlp
 
 # Commit the change only if there are changes
-if git diff --quiet poetry.lock poetry.toml; then
+if git diff --quiet poetry.lock pyproject.toml; then
     echo "No changes to commit."
 else
-    git add poetry.lock poetry.toml
+    git add poetry.lock pyproject.toml
     git commit -m "updated yt-dlp to latest"
     git push
     echo "yt-dlp updated to latest and changes pushed to remote."
