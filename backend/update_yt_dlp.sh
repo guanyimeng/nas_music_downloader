785bbaf0a124d3ee868c10174efea5262c774213
Nick Meng <mgy0822@gmail.com>
2026-02-03 01:27:06 -0500

    Commit the yt_dlp updating only if there are changes

--- backend/update_yt_dlp.sh
+++ backend/update_yt_dlp.sh
@@ -5,11 +5,13 @@ cd /home/dolphins22/Documents/Dev/nas_music_downloader/backend/
 # Update yt-dlp to latest
 poetry update yt-dlp
 
-# Commit the change
-git add poetry.lock poetry.toml
-git commit -m "updated yt-dlp to latest"
+# Commit the change only if there are changes
+if git diff --quiet poetry.lock poetry.toml; then
+    echo "No changes to commit."
+else
+    git add poetry.lock poetry.toml
+    git commit -m "updated yt-dlp to latest"
+    git push
+    echo "yt-dlp updated to latest and changes pushed to remote."
+fi
 
-# Push to remote
-git push
-
-echo "yt-dlp updated to latest and changes pushed to remote."
